b8b7b2edc10828efe32107a6ec626e88407add30
phatbuii1207 <phatphatrp@gmail.com>
2026-06-08 22:55:08 +0700

    feat: add Fan entity and helper classes for lock types and simulation results

---
+++ src/main/java/model/SimulationResult.java
@@ -0,0 +1,9 @@
+package model;
+
+/**
+ * Kết quả mô phỏng (dùng cho stress-test đặt vé đồng thời).
+ * Sẽ được hoàn thiện ở tuần sau.
+ */
+public class SimulationResult {
+    // TODO: thêm fields khi implement stress-test
+}
